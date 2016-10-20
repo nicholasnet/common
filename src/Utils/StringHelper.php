9e3a84c2a8f004e9d6bbfc411ce389534a25a13f
nicholasnet <nirmalp@hotmail.com>
2016-10-20 12:19:21 -0400

    Changed the comment positioning

--- src/Utils/StringHelper.php
+++ src/Utils/StringHelper.php
@@ -303,19 +303,12 @@ class StringHelper
     {
         return sprintf(
             '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
+            mt_rand(0, 65535), // 32 bits for "time_low"
             mt_rand(0, 65535),
-            mt_rand(0, 65535),
-            // 16 bits for "time_mid"
-            mt_rand(0, 65535),
-            // 12 bits before the 0100 of (version) 4 for "time_hi_and_version"
-            mt_rand(0, 4095) | 0x4000,
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff) | 0x8000,
-            // 48 bits for "node"
-            mt_rand(0, 65535),
+            mt_rand(0, 65535), // 16 bits for "time_mid"
+            mt_rand(0, 4095) | 0x4000, // 12 bits before the 0100 of (version) 4 for "time_hi_and_version"
+            mt_rand(0, 0x3fff) | 0x8000, // 16 bits, 8 bits for "clk_seq" hi_res and low_res two most significant bits holds zero and one for variant DCE1.1
+            mt_rand(0, 65535), // 48 bits for "node"
             mt_rand(0, 65535),
             mt_rand(0, 65535)
         );
