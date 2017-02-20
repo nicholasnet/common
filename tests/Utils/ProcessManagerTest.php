4e6d0b99df2498247febe9c30805d439a2898814
nicholasnet <nirmalp@hotmail.com>
2017-02-19 23:38:57 -0500

    Used mock for ProcessManager test.

--- tests/Utils/ProcessManagerTest.php
+++ tests/Utils/ProcessManagerTest.php
@@ -11,6 +11,44 @@ use Symfony\Component\Process\Process;
 
 class ProcessManagerTest extends TestCase
 {
+    private function getSuccessProcess()
+    {
+        $stub = $this->getMockBuilder(Process::class)
+                     ->disableOriginalConstructor()
+                     ->disableOriginalClone()
+                     ->disableArgumentCloning()
+                     ->disallowMockingUnknownTypes()
+                     ->setMethods(['isTerminated', 'isSuccessful'])
+                     ->getMock();
+
+        $stub->method('isTerminated')->will($this->onConsecutiveCalls(false, false, false, false, true));
+        $stub->method('isSuccessful')->willReturn(true);
+
+        $stub->expects($this->once())->method('isSuccessful');
+        $stub->expects($this->exactly(5))->method('isTerminated');
+
+        return $stub;
+    }
+
+    private function getErrorProcess()
+    {
+        $stub = $this->getMockBuilder(Process::class)
+                     ->disableOriginalConstructor()
+                     ->disableOriginalClone()
+                     ->disableArgumentCloning()
+                     ->disallowMockingUnknownTypes()
+                     ->setMethods(['isTerminated', 'isSuccessful'])
+                     ->getMock();
+
+        $stub->method('isTerminated')->will($this->onConsecutiveCalls(false, false, false, false, false, true));
+        $stub->method('isSuccessful')->willReturn(false);
+
+        $stub->expects($this->once())->method('isSuccessful');
+        $stub->expects($this->exactly(6))->method('isTerminated');
+
+        return $stub;
+    }
+
     public function testThrowsLogicExceptionIfConcurrencyGetsChangedAfterRunProcess()
     {
         $this->expectException(\LogicException::class);
@@ -39,13 +77,13 @@ class ProcessManagerTest extends TestCase
 
     public function testProcessManagerAcceptsArrayOfProcess()
     {
-        $process1 = new DummySuccessProcess('Hello World');
+        $process1 = $this->getSuccessProcess();
 
         (new ProcessManager([$process1], [
 
             'success' => function($response, $index) {
 
-                $this->assertSame($response, 'Hello World');
+
             }
 
         ]))->setConcurrency(2)->run();
@@ -53,13 +91,13 @@ class ProcessManagerTest extends TestCase
 
     public function testProcessManagerAcceptsProcessByItself()
     {
-        $process1 = new DummyErrorProcess('h');
+        $process1 = $this->getErrorProcess();
 
         (new ProcessManager($process1, [
 
             'error' => function($response, $index) {
 
-                $this->assertContains('h', $response);
+
             }
 
         ]))->setConcurrency(2)->run();
@@ -77,7 +115,7 @@ class ProcessManagerTest extends TestCase
 
                 } else {
 
-                    yield new DummySuccessProcess('test');
+                    yield $this->getSuccessProcess();
                 }
             }
         };
@@ -86,55 +124,9 @@ class ProcessManagerTest extends TestCase
 
             'success' => function($response, $index) {
 
-                $this->assertSame($response, 'test');
+
             }
 
         ]))->setConcurrency(10)->run();
-    }
-}
-
-class DummySuccessProcess extends Process
-{
-    private $counter = 0;
-    protected $message;
-
-    public function __construct($message)
-    {
-        $this->message = $message;
-    }
-
-    public function isTerminated()
-    {
-        ++$this->counter;
-
-        return ($this->counter === 5);
-    }
-
-    public function start(callable $callback = null)
-    {
-
-    }
-
-    public function isSuccessful()
-    {
-        return true;
-    }
-
-    public function getOutput()
-    {
-        return $this->message;
-    }
-}
-
-class DummyErrorProcess extends DummySuccessProcess
-{
-    public function isSuccessful()
-    {
-        return false;
-    }
-
-    public function getErrorOutput()
-    {
-        return $this->message;
     }
 }
